25ae3ca81608a718614d48995a95f85bf1edf851
birkan <b.561birkan@gmail.com>
2022-10-07 13:15:10 +0300

    firebase is integrated to the project

---
+++ src/utils/firebaseUtil.js
@@ -0,0 +1,33 @@
+import { initializeApp } from "firebase/app";
+import {
+  getAuth,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  signOut,
+} from "firebase/auth";
+
+const app = initializeApp({
+  apiKey: process.env.REACT_APP_apiKey,
+  authDomain: process.env.REACT_APP_authDomain,
+  projectId: process.env.REACT_APP_projectId,
+  storageBucket: process.env.REACT_APP_storageBucket,
+  messagingSenderId: process.env.REACT_APP.messagingSenderId,
+  appId: process.env.REACT_APP.appId,
+});
+
+export const auth = getAuth(app);
+
+// create new user
+export const signup = (email, password) => {
+  return createUserWithEmailAndPassword(auth, email, password);
+};
+
+// read exist user
+export const login = (email, password) => {
+  return signInWithEmailAndPassword(auth, email, password);
+};
+
+// logout process
+export const logout = () => {
+  signOut(auth);
+};
